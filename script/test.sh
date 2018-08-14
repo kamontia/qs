36c22e63d571c51c8d8fe369e9c3d07dd67d1a3e
Takeshi Kondo <chaspy@users.noreply.github.com>
2018-08-14 12:12:48 +0900

    Add test for fixup

--- script/test.sh
+++ script/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -eux
+set -o pipefail
 
 TESTDIR=test-git-fixup-$$
 
@@ -7,13 +8,32 @@ mkdir -p $TESTDIR
 cp ./git-fixup $TESTDIR
 cd $TESTDIR
 
+:
+: prepare git
+:
 git init
 git config --local user.email "git-fixup@example.com"
 git config --local user.name "git fixup"
+git commit --allow-empty -m "Initial commit"
 
+:
+: prepare test
+:
 touch fileA
 git add fileA
-git commit -m "add fileA"
+git commit -m "Add fileA"
 touch fileB
 git add fileB
-./git-fixup -f -n 1
+
+:
+: test
+:
+./git-fixup -f
+ADDED_FILE_NUM=`git diff HEAD^..HEAD --name-only | wc -l | tr -d ' '`
+if [ "$ADDED_FILE_NUM" == "2" ]; then
+  echo "*** test passed ***"
+  exit 0
+else
+  echo "*** test failed ***"
+  exit 1
+fi
