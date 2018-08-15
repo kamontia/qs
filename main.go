e9b934fa4dc5d601c59ededf9a597002d22934c2
Tatsuya Kamohara <17017563+kamontia@users.noreply.github.com>
2018-08-15 23:59:41 +0900

    Update to aggregate some commits into one commits

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os/exec"
 	"regexp"
 	"strconv"
+	"strings"
 
 	"github.com/codegangsta/cli"
 )
@@ -38,6 +39,41 @@ func main() {
 		var force bool = c.Bool("force")
 		var num string = strconv.Itoa(c.Int("number"))
 		var stdin string
+		var rangeArray []string
+		var iBreakNumber int
+		iNum, _ := strconv.Atoi(num)
+		var error error
+
+		/* Pick up squash range */
+		/* TODO: Check error strictly */
+		for _, v := range os.Args {
+			if strings.Contains(v, "..") {
+				rangeArray = strings.Split(v, "..")
+				iNum, error = strconv.Atoi(rangeArray[0])
+				if error != nil {
+
+				}
+				iBreakNumber, error = strconv.Atoi(rangeArray[1])
+				if error != nil {
+					fmt.Println(error)
+					os.Exit(1)
+				}
+
+				if iNum < iBreakNumber {
+					tmp := iNum
+					iNum = iBreakNumber
+					iBreakNumber = tmp
+				}
+
+				break
+			}
+		}
+
+		if len(rangeArray) == 0 {
+			fmt.Println("ERROR: argument erorr")
+			os.Exit(1)
+		}
+		/* (END) Pick up squash range */
 
 		if force {
 			fmt.Println("*** force update ***")
@@ -63,9 +99,8 @@ func main() {
 				break
 			}
 		}
-
 		// Parse number(--number, -n) parameter
-		switch number := c.Int("number"); number {
+		switch iNum {
 		case 0:
 			// fix up commit
 			fmt.Println("*** git commit --fixup ***")
@@ -93,6 +128,7 @@ func main() {
 			var commitHashList []string
 			var commitMsg []string
 			var commitNewMsg []string
+			var reflogHashList []string
 			/* Get commit hash */
 			out, err := exec.Command("git", "log", "--oneline", "--format=%h").Output()
 			if err != nil {
@@ -105,6 +141,18 @@ func main() {
 				commitHashList = append(commitHashList, v)
 			}
 			/* (END)Get commit hash */
+
+			/* Get reflog hash */
+			out, err = exec.Command("git", "reflog", "--format=%h").Output()
+			if err != nil {
+				fmt.Print(string(out))
+				fmt.Print(err)
+				os.Exit(1)
+			}
+			for _, v := range regexp.MustCompile("\r\n|\n|\r").Split(string(out), -1) {
+				reflogHashList = append(reflogHashList, v)
+			}
+			/* (END)Get reflog hash */
 
 			/* Get commit message */
 			out, err = exec.Command("git", "log", "--oneline", "--format=%s").Output()
@@ -122,8 +170,8 @@ func main() {
 			/* Display commit hash and message. The [pickup|..] strings is colored */
 			for i := len(commitMsg) - 1; i >= 0; i-- {
 				/* (WIP) Switch output corresponded to do squash */
-				if c.Int("number") > i {
-					fmt.Printf("[%2d] \x1b[35mpickup\x1b[0m -> \x1b[36msquash\x1b[0m %s %s\n", i, commitHashList[i], commitNewMsg[number])
+				if iNum > i && i >= iBreakNumber {
+					fmt.Printf("[%2d] \x1b[35mpickup\x1b[0m -> \x1b[36msquash\x1b[0m %s %s\n", i, commitHashList[i], commitNewMsg[iNum])
 				} else {
 					fmt.Printf("[%2d] \x1b[35mpickup\x1b[0m -> \x1b[35mpickup\x1b[0m %s %s\n", i, commitHashList[i], commitMsg[i])
 				}
@@ -134,14 +182,20 @@ func main() {
 			/**
 			git rebase HEAD~N --exec="git commit -m"squash! commit messages" "
 			*/
-			log.Println("Logged display")
 			/* Suppress vim editor launching */
 			os.Setenv("GIT_EDITOR", ":")
 			/* (END) Suppress vim editor launching */
-			for i := number; i >= 0; i-- {
-				speciedHead := fmt.Sprintf("HEAD~%d", i)
-				speciedExec := fmt.Sprintf("--exec=git commit --amend -m\"%s\"", commitNewMsg[number])
+			for i := iNum; i >= 0; i-- {
+				speciedHead := fmt.Sprintf("HEAD~%d", i+1)
+				var speciedExec string
+				if iNum > i && i >= iBreakNumber {
+					speciedExec = fmt.Sprintf("--exec=git commit --amend -m\"%s\"", commitNewMsg[iNum])
+				} else {
+					speciedExec = fmt.Sprintf("--exec=git commit --amend -m\"%s\"", commitMsg[i])
+				}
+
 				cmd := exec.Command("git", "rebase", speciedHead, speciedExec)
+				log.Printf("git rebaes HEAD~%d %s\n", i, speciedExec)
 
 				cmd.Stdin = os.Stdin
 				cmd.Stdout = os.Stdout
@@ -153,9 +207,8 @@ func main() {
 			}
 			/* (END) git rebase */
 
-			log.Println("Logged display")
 			/* git rebase with autosquash option */
-			speciedHead := fmt.Sprintf("HEAD~%d", number+1)
+			speciedHead := fmt.Sprintf("HEAD~%d", iNum+1)
 			cmd := exec.Command("git", "rebase", "-i", "--autosquash", "--autostash", speciedHead)
 			// Transfer the command I/O to Standard I/O
 			cmd.Stdin = os.Stdin
