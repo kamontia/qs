22a9391cbd7a8df135be13e3b79467aeb51027c7
Tatsuya Kamohara <17017563+kamontia@users.noreply.github.com>
2018-09-16 20:26:13 +0900

    Update file structure

---
+++ model/data.go
@@ -0,0 +1,10 @@
+package model
+
+type GitCommitInfo struct {
+	RangeArray             []string
+	CommitHashList         []string
+	CommitMsgList          []string
+	CommitNewMsgList       []string
+	CommitSpecifiedMsgList []string
+	ReflogHashList         []string
+}
